8f6f58b6c4f05c29ffc3c7cc9731f7f8f9aa6146
daytonpe <daytonpe14@gmail.com>
2018-02-27 14:22:08 -0600

    move normalizer to its own module

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 var express = require("express");
 var mysql = require("mysql");
 var fs = require("fs");
+var normalizer = require("./normalizer");
 
-//NORMALIZE THE BORROWER DATA
-var borrowerArr = fs
-  .readFileSync("./borrowers.csv")
-  .toString()
-  .split("\n");
-var borrowerData = [];
-for (var i = 1; i < borrowerArr.length - 1; i++) {
-  var borrower = borrowerArr[i].split(",");
-  var Ssn = borrower[1];
-  var Bname = borrower[2] + " " + borrower[3];
-  var Address = borrower[5] + ", " + borrower[6] + ", " + borrower[7];
-  var Phone = borrower[8].split("\r")[0];
-  var normBorrower = [];
-  normBorrower.push(Ssn);
-  normBorrower.push(Bname);
-  normBorrower.push(Address);
-  normBorrower.push(Phone);
-  borrowerData.push(normBorrower);
-}
+//NORMALIZE THE DATA
+borrowerData = normalizer.normalizeBorrowers();
 
 //SET UP THE DATABASE CREDENTIALS
 var con = mysql.createConnection({
